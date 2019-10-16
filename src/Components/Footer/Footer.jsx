014a08449fbf0c26b1a737bdd3022a9ad61e2f84
Ihar Trymailau <ihar.trymailau@gmail.com>
2019-10-16 20:47:35 +0300

    add logo (skills, contacts)

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import s from './Footer.module.css';
+import google from '../../images/google.svg';
+import github from '../../images/github.svg';
+import linkedin from '../../images/linkedin.svg';
+import telegram from '../../images/telegram.svg';
 
 const Footer = () => {
     return (
@@ -7,12 +11,12 @@ const Footer = () => {
             <div className={s.container}>
                 <h2>Игорь Тримайлов</h2>
                 <div className={s.contact}>
-                    <img className={s.contactImg} />
-                    <img className={s.contactImg} />
-                    <img className={s.contactImg} />
-                    <img className={s.contactImg} />
+                    <a href='https://www.ihar.trymailau@gmail.com'><img className={s.contactImg} src={google} alt='google-plus'/></a>
+                    <a href='https://github.com/ArgonTrime'><img className={s.contactImg} src={github} alt='github'/></a>
+                    <a href='https://www.linkedin.com/in/ihar-trymailau-3a778018a/'><img className={s.contactImg} src={linkedin} alt='linkedin'/></a>
+                    <img className={s.contactImg} src={telegram} alt='telegram'/>
                 </div>
-                <span>@2019 Все права защищены</span>
+                <span>©2019 Все права защищены</span>
             </div>
         </div>
     )
--- src/Components/MySkills/MySkills.jsx
+++ src/Components/MySkills/MySkills.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import s from './MySkills.module.css';
 import TitleBlock from "../TitleBlock/TitleBlock";
+import htmlCss from '../../images/html-css.svg';
+import js from '../../images/js.svg';
+import react from '../../images/react.png';
+
+
 
 const MySkills = (props) => {
     return (
@@ -9,21 +14,21 @@ const MySkills = (props) => {
                 <TitleBlock title={props.skills.title}/>
                 <div className={s.skills}>
                     <div className={s.skill}>
-                        <img className={s.imgSkill}/>
+                        <img className={s.imgSkill} src={htmlCss} alt='html-css'/>
                         <TitleBlock title={props.skills.h}/>
                         <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
                             ut labore et dolore magna aliqua. Nunc non blandit massa enim. Lacus sed viverra tellus in
                             hac habitasse platea dictumst. </span>
                     </div>
                     <div className={s.skill}>
-                        <img className={s.imgSkill}/>
+                        <img className={s.imgSkill} src={js} alt='javascript'/>
                         <TitleBlock title={props.skills.j}/>
                         <span>Egestas sed sed risus pretium quam vulputate. Rhoncus aenean vel elit scelerisque mauris
                             pellentesque pulvinar. Amet porttitor eget dolor morbi non arcu risus quis varius. Posuere
                             morbi leo urna molestie at elementum eu facilisis.</span>
                     </div>
                     <div className={s.skill}>
-                        <img className={s.imgSkill}/>
+                        <img className={s.imgSkill} src={react} alt='react'/>
                         <TitleBlock title={props.skills.r}/>
                         <span>Aliquam faucibus purus in massa. Etiam sit amet nisl purus in mollis nunc sed id.
                             Tortor id aliquet lectus proin nibh nisl condimentum id. Purus semper eget duis at.
