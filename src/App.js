a4f65398eac270510176324da05ae08b117679e3
Ihar Trymailau <ihar.trymailau@gmail.com>
2019-10-27 19:06:16 +0300

    add reactt-particles and reveal

--- src/App.js
+++ src/App.js
@@ -7,8 +7,21 @@ import Projects from "./Components/Projects/Projects";
 import Slogan from "./Components/Slogan/Slogan";
 import Contacts from "./Components/Contacts/Contacts";
 import Footer from "./Components/Footer/Footer";
+import Particles from 'react-particles-js';
 
 class App extends React.Component {
+    particlesOpt = {
+        particles: {
+            number: {
+                value: 150,
+                density: {
+                    enable: true,
+                    value_area: 800
+
+                }
+            }
+        }
+    };
     titleBlock = {
         skills: {
             title: 'Мои навыки',
@@ -30,12 +43,17 @@ class App extends React.Component {
         return (
             <div className='App'>
                 <div className='portfolio'>
+                    <Particles className='particles'
+                               params={this.particlesOpt}/>
                     <Header/>
                     <MainInfo/>
                     <MySkills skills={this.titleBlock.skills}/>
-                    <Projects project={this.titleBlock.project} btn={this.titleBlock.button}/>
-                    <Slogan work={this.titleBlock.work} btn={this.titleBlock.button}/>
-                    <Contacts contacts={this.titleBlock.contacts} btn={this.titleBlock.button}/>
+                    <Projects project={this.titleBlock.project}
+                              btn={this.titleBlock.button}/>
+                    <Slogan work={this.titleBlock.work}
+                            btn={this.titleBlock.button}/>
+                    <Contacts contacts={this.titleBlock.contacts}
+                              btn={this.titleBlock.button}/>
                     <Footer/>
                 </div>
             </div>
